84bb98e5346fcfff2df948ef856146a86f71c983
clearbusinessinsight <choltz@cacbydesign.com>
2022-11-30 08:08:01 -0500

    complete

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -32,28 +32,3 @@ window.addEventListener('appinstalled', (event) => {
     console.log('install hit')
     window.deferredPrompt = null;
 }); 
-
-// const butInstall = document.getElementById('buttonInstall');
-// window.addEventListener('beforeinstallprompt', (event) => {
-//     console.log('hit');
-//     console.log("event" + event);
-//     event.preventDefault();
-//     window.deferredPrompt = event;
-//     butInstall.classList.toggle('hidden', false);
-// });
-
-// butInstall.addEventListener('click', async () => {
-//     const promptEvent = window.deferredPrompt;
-//      if (!promptEvent) {
-//         return;
-//     };
-//     promptEvent.prompt();
-//     window.deferredPrompt = null;
-//     butInstall.classList.toggle('hidden', true);
-// });
-
-// window.addEventListener('appinstalled', (event) => {
-//     // Clear prompt
-//     console.log('install hit');
-//     window.deferredPrompt = null;
-// }); 
